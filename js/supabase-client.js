e7617fd079b543cdf2eb04908cc36cbaadbc5e63
Euwan Abogadie <yoowan28@gmail.com>
2026-02-18 11:20:02 +0800

    new

--- js/supabase-client.js
+++ js/supabase-client.js
@@ -1,5 +1,5 @@
 const SUPABASE_URL = 'https://gyrqdwypsqdeeqzmkdze.supabase.co';
-const SUPABASE_ANON_KEY = 'PASTE_YOUR_FULL_ANON_KEY_HERE';
+const SUPABASE_ANON_KEY = 'sb_publishable_MHxJDqikMx6GI_Ctbf9eeQ_Tt7Q7Uk5';
 
 const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
