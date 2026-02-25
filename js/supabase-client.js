8a10ee4d45ea71bde6e5c2f71ade10acb06329d7
Euwan Abogadie <yoowan28@gmail.com>
2026-02-25 09:58:41 +0800

    cooler

--- js/supabase-client.js
+++ js/supabase-client.js
@@ -1,6 +1,6 @@
 const SUPABASE_URL = 'https://gyrqdwypsqdeeqzmkdze.supabase.co';
-const SUPABASE_ANON_KEY = 'sb_publishable_MHxJDqikMx6GI_Ctbf9eeQ_Tt7Q7Uk5';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd5cnFkd3lwc3FkZWVxem1rZHplIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzEzNjc0MTMsImV4cCI6MjA4Njk0MzQxM30.vR16sz35XJ9wx7HNf-RVeZQ2RoWLeRwgclePx2VT9RA';
 
 const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
-console.log('Supabase connected:', supabase);
+console.log('Supabase connected!');
